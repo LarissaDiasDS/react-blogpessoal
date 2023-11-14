5a6e2f16ddaa03a624b27951060470f161760415
LarissaDiasDS <larissalari174@gmail.com>
2023-11-13 22:41:25 -0300

    Tema blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import Login from './pages/login/Login';
 import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import Cadastro from './pages/cadastro/Cadastro';
+import ListaTemas from './components/tema/listaTemas/ListaTemas';
+import FormularioTema from './components/tema/formularioTema/FormularioTema';
+import DeletarTema from './components/tema/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+
 
 function App() {
     return (
@@ -18,6 +23,11 @@ function App() {
                         <Route path="/login" element={<Login />} />
                         <Route path="/home" element={<Home />} />
                         <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastroTema" element={<FormularioTema />} />
+                        <Route path="/editarTema/:id" element={<FormularioTema />} />
+                        <Route path="/deletarTema/:id" element={<DeletarTema />} />
+                        <Route path="/postagens" element={<ListaPostagens />} />
                     </Routes>
                 </div>
                 <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -20,9 +20,9 @@ function Navbar() {
                 <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
 
                 <div className='flex gap-4'>
-                    Postagens
-                    Temas
-                    Cadastrar tema
+                    <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                    <Link to='/temas' className='hover:underline'>Temas</Link>
+                    <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
                     Perfil
                     <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                 </div>
